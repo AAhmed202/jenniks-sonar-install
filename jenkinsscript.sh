4cfe21f340cd3dba87a872ddb1b2f2955603b485
AAhmed202 <73727185+AAhmed202@users.noreply.github.com>
2023-10-30 17:01:05 +0000

    install-jenkins script

---
+++ jenkinsscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install Jenkins on the VM
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install -y openjdk-11-jre
+
+
+sudo curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key \
+        | sudo tee /usr/share/keyrings/jenkins-keyring.asc > \
+        /dev/null
+
+sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+        https://pkg.jenkins.io/debian binary/ | \
+        sudo tee /etc/apt/sources.list.d/jenkins.list > \
+        /dev/null
+
+
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+sudo apt-get install git docker.io
+
+curl http://localhost:8080
